d5100e24ecf1f2fc7795165f7b17991e601e8d44
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2023-07-02 22:22:00 +0200

    Skip writing to file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eo pipefail
 
-java -jar ftl-tests-results-r8.jar --merged-results="$INPUT_MERGED_RESULT_FILE" > markdown_summary
+summary=$(java -jar ftl-tests-results-r8.jar --merged-results="$INPUT_MERGED_RESULT_FILE")
 
 delimiter=$(openssl rand -hex 20)
 echo "summary-markdown<<$delimiter" >> $GITHUB_OUTPUT
-cat markdown_summary >> $GITHUB_OUTPUT
+echo "$summary" >> $GITHUB_OUTPUT
 echo "$delimiter" >> $GITHUB_OUTPUT
 
-cat markdown_summary >> $GITHUB_STEP_SUMMARY
+echo "$summary" >> $GITHUB_STEP_SUMMARY
