52fe1a94fa073ee2cdca9047d1584637dcdc700d
Łukasz Wasylkowski <l.wasyl@gmail.com>
2023-07-02 21:55:18 +0200

    Fix action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eo pipefail
 
-summary=$(java -jar ftl-tests-results-r8.jar --merged-results="$INPUT_MERGED_RESULT_FILE")
+java -jar ftl-tests-results-r8.jar --merged-results="$INPUT_MERGED_RESULT_FILE" > markdown_summary
 
 delimiter=$(openssl rand -hex 20)
 echo "summary-markdown<<$delimiter" >> $GITHUB_OUTPUT
-echo "$summary" >> $GITHUB_OUTPUT
+cat markdown_summary >> $GITHUB_OUTPUT
 echo "$delimiter" >> $GITHUB_OUTPUT
 
-echo $summary >> $GITHUB_STEP_SUMMARY
+cat markdown_summary >> $GITHUB_STEP_SUMMARY
