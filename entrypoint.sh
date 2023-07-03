369435ab9b54235b3710fedcc14026e8e86cdfa5
Łukasz Wasylkowski <l.wasyl@gmail.com>
2023-07-03 15:38:51 +0200

    Don't post markdown update when there are no failures

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,11 @@ set -eo pipefail
 
 summary=$(java -jar ftl-tests-results-r8.jar --merged-results="$INPUT_MERGED_RESULT_FILE")
 
-delimiter=$(openssl rand -hex 20)
-echo "summary-markdown<<$delimiter" >> $GITHUB_OUTPUT
-echo "$summary" >> $GITHUB_OUTPUT
-echo "$delimiter" >> $GITHUB_OUTPUT
+if [ ! -z "failures" ]; then
+    delimiter=$(openssl rand -hex 20)
+    echo "summary-markdown<<$delimiter" >> $GITHUB_OUTPUT
+    echo "$summary" >> $GITHUB_OUTPUT
+    echo "$delimiter" >> $GITHUB_OUTPUT
+fi
 
 echo "$summary" >> $GITHUB_STEP_SUMMARY
