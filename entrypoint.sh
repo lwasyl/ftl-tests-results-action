8ae2acdc0dd956b855ed46060dc21d582a6f825c
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2023-07-03 15:26:26 +0200

    asdasd

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,11 @@ mv build/libs/ftl-tests-results-r8.jar ftl-tests-results-r8.jar
 summary=$(java -jar ftl-tests-results-r8.jar summary --merged-results="$INPUT_MERGED_RESULT_FILE")
 failures=$(java -jar ftl-tests-results-r8.jar ghComment --merged-results="$INPUT_MERGED_RESULT_FILE")
 
-delimiter=$(openssl rand -hex 20)
-echo "summary-markdown<<$delimiter" >> $GITHUB_OUTPUT
-echo "$failures" >> $GITHUB_OUTPUT
-echo "$delimiter" >> $GITHUB_OUTPUT
+if [ ! -z "failures" ]; then
+  delimiter=$(openssl rand -hex 20)
+  echo "summary-markdown<<$delimiter" >> $GITHUB_OUTPUT
+  echo "$failures" >> $GITHUB_OUTPUT
+  echo "$delimiter" >> $GITHUB_OUTPUT
+fi
 
 echo "$summary" >> $GITHUB_STEP_SUMMARY
