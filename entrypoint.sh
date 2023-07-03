4db9b38c53da0879bed72ae5f6341ccd56cedfaf
Łukasz Wasylkowski <3951580+lwasyl@users.noreply.github.com>
2023-07-03 16:14:27 +0200

    Revert "Don't post markdown update when there are no failures"
    This reverts commit 13ec4de8d0153a8656ca311ce6b5dee9883434fd.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,9 @@ set -eo pipefail
 
 summary=$(java -jar ftl-tests-results-r8.jar --merged-results="$INPUT_MERGED_RESULT_FILE")
 
-if [ ! -z "failures" ]; then
-    delimiter=$(openssl rand -hex 20)
-    echo "summary-markdown<<$delimiter" >> $GITHUB_OUTPUT
-    echo "$summary" >> $GITHUB_OUTPUT
-    echo "$delimiter" >> $GITHUB_OUTPUT
-fi
+delimiter=$(openssl rand -hex 20)
+echo "summary-markdown<<$delimiter" >> $GITHUB_OUTPUT
+echo "$summary" >> $GITHUB_OUTPUT
+echo "$delimiter" >> $GITHUB_OUTPUT
 
 echo "$summary" >> $GITHUB_STEP_SUMMARY
